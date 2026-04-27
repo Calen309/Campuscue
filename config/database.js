f2703058c012bd093a2153ed01a7a47c14ac90e3
Calen309 <calen.davis@go.tarleton.edu>
2026-04-27 16:48:32 -0500

    Create SQLite database setup

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// SQLite database setup and initialization
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('campuscue.db', (err) => {
+    if (err) {
+        console.error('Error opening database ' + err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+});
+
+module.exports = db;
